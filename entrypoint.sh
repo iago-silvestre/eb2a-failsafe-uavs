9e827db8a38159dec61b8d02d414c12f47f5d94e
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-04-13 20:35:52 -0300

    updated entrypoint to add chmod on gradlew

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 # Optional: pull latest changes
 cd /root/catkin_ws/src/eb2a-failsafe-uavs && git pull
 
-# Make sure the firefight script is executable
+# Make sure the firefight start script and agents are executable
 chmod +x /root/catkin_ws/src/eb2a-failsafe-uavs/tmux/firefight/start.sh
+chmod +x /root/catkin_ws/src/eb2a-failsafe-uavs/tmux/firefight/Jason/gradlew
 
 # Copy your world file
 cp /root/catkin_ws/src/eb2a-failsafe-uavs/worlds/fireTree.world /opt/ros/noetic/share/mrs_gazebo_common_resources/worlds/
