72ac6386add7722b53a33b6927ec6a0ff778827b
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-04-13 21:46:26 -0300

    added firetemp.py to chmod list

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ cd /root/catkin_ws/src/eb2a-failsafe-uavs && git pull
 # Make sure the firefight start script and agents are executable
 chmod +x /root/catkin_ws/src/eb2a-failsafe-uavs/tmux/firefight/start.sh
 chmod +x /root/catkin_ws/src/eb2a-failsafe-uavs/tmux/firefight/Jason/gradlew
+chmod +x /root/catkin_ws/src/eb2a-failsafe-uavs/tmux/firefight/fireTemp.py
 
 # Copy your world file
 cp /root/catkin_ws/src/eb2a-failsafe-uavs/worlds/fireTree.world /opt/ros/noetic/share/mrs_gazebo_common_resources/worlds/
